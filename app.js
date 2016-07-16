7ec7da0a159d5d424fdb06a9742a5d00916ee033
yedaodao <404069912@qq.com>
2016-07-16 16:56:09 +0800

    add connection test

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var koa = require('koa');
+var http = require('http');
 var path = require('path');
 var koaStatic = require('koa-static');
 
 var app = koa(),
 // 需要启动的app名称（bundle的子文件夹名称)
-    startApps = ['core', 'hang'];
+    startApps = ['core', 'hang'],
+    connects = 0;
 
 // app middleware
 app.use(koaStatic('./static'));
@@ -16,4 +18,26 @@ startApps.forEach(function (bundleName) {
     bundle.start(app);
 });
 
-app.listen(3000);
+var server = http.createServer(app.callback());
+server.listen(3000);
+
+server.on('connection', function () {
+    console.log('connection');
+    server.getConnections(function (err, count) {
+        console.log(count);
+    });
+});
+
+
+process.on('SIGTERM', function () {
+    console.log('exit code');
+    server.close(function () {
+        server.getConnections(function (err, count) {
+            console.log(count);
+        });
+        console.log('http server stop');
+    });
+});
+process.on('exit', function () {
+    console.log('exit');
+});
